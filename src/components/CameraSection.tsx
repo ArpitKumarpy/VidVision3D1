35a4ea8bf16774fb87c036103640bc1fb77604c2
Arpit Kumar <114872072+ArpitKumarpy@users.noreply.github.com>
2025-08-09 20:11:49 +0530

    Update CameraSection.tsx

--- src/components/CameraSection.tsx
+++ src/components/CameraSection.tsx
@@ -94,7 +94,7 @@ const CameraSection: React.FC<CameraSectionProps> = ({ onLandmarksReceived }) =>
       const formData = new FormData();
       formData.append('video', recordedBlob, 'recorded-video.webm');
 
-      const response = await fetch('http://localhost:5000/upload', {
+      const response = await fetch('https://vidvision3d-backend.onrender.com/upload', {
         method: 'POST',
         body: formData,
       });
@@ -106,9 +106,9 @@ const CameraSection: React.FC<CameraSectionProps> = ({ onLandmarksReceived }) =>
       const data = await response.json();
       
       const [bodyData, leftHandData, rightHandData] = await Promise.all([
-        fetch(`http://localhost:5000${data.body_landmarks}`).then(res => res.text()),
-        fetch(`http://localhost:5000${data.left_hand_landmarks}`).then(res => res.text()),
-        fetch(`http://localhost:5000${data.right_hand_landmarks}`).then(res => res.text())
+        fetch(`https://vidvision3d-backend.onrender.com${data.body_landmarks}`).then(res => res.text()),
+        fetch(`https://vidvision3d-backend.onrender.com${data.left_hand_landmarks}`).then(res => res.text()),
+        fetch(`https://vidvision3d-backend.onrender.com${data.right_hand_landmarks}`).then(res => res.text())
       ]);
 
       onLandmarksReceived({
